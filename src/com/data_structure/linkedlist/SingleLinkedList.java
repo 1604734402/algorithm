ab4a81c95a47d1c5f9734358d88a37b5bf05a3cb
lym <1604734402@qq.cpm>
2020-11-20 23:17:57 +0800

    P22 单链表的翻转，利用额外的节点来保存下一个节点，避免断开

--- src/com/data_structure/linkedlist/SingleLinkedList.java
+++ src/com/data_structure/linkedlist/SingleLinkedList.java
@@ -8,6 +8,10 @@ public class SingleLinkedList {
     //初始化头结点，不存放具体数据，用于管理此链表
     private HeroNode head = new HeroNode(0, "", "");
 
+    public HeroNode getHead() {
+        return head;
+    }
+
     /**
      * 添加节点到单项链表
      * <p>
@@ -31,14 +35,11 @@ public class SingleLinkedList {
 
     /**
      * 根据Node的no属性来进行顺序插入
-     * <p>
      * 1、首先找到要新提那家节点的位置,通过辅助变量
      * 2、新的节点的next等于辅助节点的next域
      * 3、将辅助节点的next域指向新加入节点
-     * <p>
      * 因为头结点不能动，因此我们仍然通过一个辅助指针(变量)来帮助找到添加的位置
      * 由于是单链表，而我们找的temp是位于添加位置的前一个节点，否则插入不了
-     *
      * @param heroNode
      */
     public void addByOrder(HeroNode heroNode) {
@@ -161,5 +162,34 @@ public class SingleLinkedList {
             temp = temp.next;
         }
     }
+
+    /**
+     * 翻转单链表
+     * @param head
+     * @return
+     */
+    public HeroNode reverLinkedList(HeroNode head){
+        if (head == null || head.next == null){
+            return null;
+        }
+        //新建临时存储翻链表
+        HeroNode reverNode = new HeroNode(0,null,null);
+        HeroNode temp = head.next;
+        HeroNode cur = new HeroNode(111,"111","111");
+
+
+        while (temp!=null){
+            cur = temp.next;
+            temp.next =reverNode.next;
+            //经过上一步后，temp.next就已经等于Null了
+            reverNode.next = temp;
+            //但cur相当于是一个新的节点，为了避免链表断开
+            temp = cur;
+
+        }
+        head.next = reverNode.next;
+        return head;
+    }
+
 
 }
