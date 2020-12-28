1dc2b1039401d45ce49968a5b7ab8f01896a1e64
home <1604734402@qq.com>
2020-12-28 22:23:33 +0800

    P72 快速排序 归并排序

---
+++ src/com/algorithm/sort/MergeSort.java
@@ -0,0 +1,151 @@
+package com.algorithm.sort;
+
+import com.common.Util;
+
+/**
+ * 归并排序
+ * 利用归并的思想实现的排序方法，该算法采用经典的分治策略
+ * 递归拆分子序列
+ * 分治(分而治之)：
+ * 分治法将问题分成一些小的问题然后递归求解，而治的阶段则将分的阶段得到的答案“修补”在一起
+ */
+public class MergeSort {
+    static int curCount = 0;
+    static int curCount1 = 0;
+
+    public static void main(String[] args) {
+        int[] arr = {8, 4, 5, 7, 1, 3, 6, 2};
+        int[] temp = new int[arr.length];
+        mergeSort(arr,0,arr.length-1,temp);
+        Util.printArray(arr);
+
+    }
+
+    public static void mergeSort(int[] arr,int left,int right,int[] temp){
+        System.out.println("curCount1:"+curCount1++);
+        if (left<right){
+            int mid = (left+right)/2;//中间索引
+            //左递归进行分解
+            mergeSort(arr,left,mid,temp);
+            //右递归进行分解机
+            mergeSort(arr,mid+1,right,temp);
+
+            merge(arr,left,mid,right,temp);
+        }
+
+
+    }
+
+
+    /**
+     * @param arr   需要排序的原始数组
+     * @param left  左边序列的初始下标
+     * @param mid   中间件索引
+     * @param right 右边索引
+     * @param temp  中转数组
+     */
+    public static void merge(int[] arr, int left, int mid, int right, int[] temp) {
+        System.out.println("curCont:"+curCount++);
+
+        int i = left;//初始化i ,左边有序序列的初始索引
+        int j = mid + 1;//初始化j,右边有序序列的初始索引
+        int t = 0;//临时数组(temp)的初始索引
+
+        //先把左右两边的(有序)的数据安装规则填充到temp数组
+        //直到左右两边的有序序列，有一边处理完毕为止
+        while (i <= mid && j <= right) {
+            //如果左边的有序序列当前元素，小于等于右边有序序列的当前元素
+            //即将左边的元素,拷贝到temp数组
+            //t后移，i后移
+            if (arr[i] <= arr[j]) {
+                temp[t] = arr[i];
+                t++;
+                i++;
+            }else {
+                temp[t] = arr[j];
+                t++;
+                j++;
+            }
+
+        }
+
+        //把有剩余数据的一边的数据局级依次全部填充到temp
+
+        while (i <= mid) {
+            temp[t] = arr[i];
+            t++;
+            i++;
+        }
+
+        while (j <= right) {
+            temp[t] = arr[j];
+            t++;
+            j++;
+        }
+
+        //将temp数组中排好序的元素拷贝到arr
+        t = 0;
+        int temLeft = left;
+        while (temLeft <= right) {
+            arr[temLeft] = temp[t];
+            t++;
+            temLeft++;
+        }
+
+    }
+
+
+    /**
+     * @param arr   需要排序的原始数组
+     * @param left  左边序列的初始下标
+     * @param mid   中间件索引
+     * @param right 右边索引
+     * @param temp  中转数组
+     */
+    public static void merge1(int[] arr, int left, int mid, int right, int[] temp) {
+
+        int i = left;//初始化i ,左边有序序列的初始索引
+        int j = mid + 1;//初始化j,右边有序序列的初始索引
+        int t = 0;//临时数组(temp)的初始索引
+
+        //先把左右两边的(有序)的数据安装规则填充到temp数组
+        while (i <= mid && j <= right) {
+            //如果左边的有序序列当前元素，小于等于右边有序序列的当前元素
+            //即将左边的元素,拷贝到temp数组
+            //t后移，i后移
+            if (arr[i] <= arr[j]) {
+                temp[t] = arr[i];
+                t++;
+                i++;
+            } else {
+                temp[t] = arr[j];
+                t++;
+                j++;
+            }
+        }
+
+        //直到左右两边的有序序列，有一边处理完毕为止
+
+
+        //把有剩余数据的一边的数据局级依次全部填充到temp
+        while (i <= mid) {
+            temp[t] = arr[i];
+            t++;
+            i++;
+        }
+
+        while (j <= right) {
+            temp[t] = arr[j];
+            t++;
+            j++;
+        }
+
+
+        //将temp数组的元素拷贝到arr
+        for (int k = 0; k < arr.length; k++) {
+            arr[k] = temp[k];
+        }
+
+    }
+
+}
---
+++ src/com/algorithm/sort/QuickSort.java
@@ -0,0 +1,233 @@
+package com.algorithm.sort;
+
+import com.common.Util;
+
+/**
+ * 快速排序
+ * 选定Pivot中心轴
+ * 将大于Pivot的数字放在Pivot的右边
+ * 将小于Pivot的数字放在Pivot的左边
+ * 分别对左右子序列重复前三步操作
+ */
+public class QuickSort {
+
+    public static void main(String[] args) {
+        int[] arr = {-9, 78, 0, 0, -567, 0};
+        testqquickSort(arr, 0, arr.length - 1);
+//        quickSortFirst(arr, 0, arr.length - 1);
+        Util.printArray(arr);
+    }
+
+    /**
+     * 以中间的值为基准
+     *
+     * @param arr
+     * @param left
+     * @param right
+     */
+    public static void quickSortPrivot(int[] arr, int left, int right) {
+        //左下标
+        int l = left;
+        //右下标
+        int r = right;
+        //中轴值
+        int privot = arr[(left + right) / 2];
+
+        //while 循环的目的是让比pivot 值小的放到左边,比Pivot大的值放到右边，进行交换
+        while (l < r) {
+            //在Pivot的左边一直找，找到一个大于等于pivot的值，退出
+            while (arr[l] < privot) {
+                l += 1;
+            }
+            //在pivot的右边一直找，找到一个小于等于pivot的值，退出
+            while (arr[r] > privot) {
+                r -= 1;
+            }
+            //如果l >=r 说明pivot的左右两值，左边都小于pivot值，右边都大于pivot值
+            if (l >= r) {
+                break;
+            }
+            //进行交换
+            int temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+
+            //如果交换完后，发现arr[1] == pivot值 相等--,前移
+            if (arr[l] == privot) {
+                r--;
+            }
+            //如果交换完后，发现arr[r] == pivot值 相等l++,后移
+            if (arr[r] == privot) {
+                l++;
+            }
+
+        }
+
+        if (l == r) {
+            l++;
+            r--;
+        }
+
+        if (left < r) {
+            quickSortPrivot(arr, left, r);
+        }
+
+        if (l < right) {
+            quickSortPrivot(arr, l, right);
+        }
+
+
+    }
+
+    /**
+     * 以第一个数做基准
+     * 从前往后开始
+     *
+     * @param arr
+     * @param left
+     * @param right
+     */
+    public static void quickSortFirst(int[] arr, int left, int right) {
+
+        if (left >= right) {
+            return;
+        }
+
+        int l = left;
+        int r = right;
+        int pivot = arr[left];
+
+        //l<r 保证不越界 双指针
+        while (l < r) {
+
+            //l <r 保证不越界
+            //arr[r]>=pivot从右往左寻找小于pivot的值
+            while (l < r && arr[r] >= pivot) {
+                r--;
+            }
+            //如果说寻找到下标r的数大于下标l的数，则进行赋值
+            if (l < r) {
+                arr[l] = arr[r];
+            }
+
+            //从左往右寻找大于pivot的值
+            while (l < r && arr[l] <= pivot) {
+                l++;
+            }
+            //如果说寻找到下标l的值小于下标r的值，则进行赋值
+            if (l < r) {
+                arr[r] = arr[l];
+            }
+        }
+        //当退出循环时，则证明找到合适位置，将预先保存的pivot值赋值
+        arr[l] = pivot;
+
+
+        //左递归
+        if (r > left) {
+            quickSortFirst(arr, left, r - 1);
+        }
+
+        //右递归
+        if (l < right) {
+            quickSortFirst(arr, l + 1, right);
+        }
+
+
+    }
+
+    /**
+     * 以最后一个数做基准
+     * 从后往前开始
+     *
+     * @param arr
+     * @param left
+     * @param right
+     */
+    public static void quickSortLast(int[] arr, int left, int right) {
+
+        if (left >= right) {
+            return;
+        }
+
+        int l = left;
+        int r = right;
+        int pivot = arr[right];
+
+        while (l < r) {
+
+
+            while (l < r && arr[l] <= pivot) {
+                l++;
+            }
+            if (r > l) {
+                arr[r] = arr[l];
+            }
+
+            while (l < r && arr[r] >= pivot) {
+                r--;
+            }
+
+            if (r > l) {
+                arr[l] = arr[r];
+            }
+
+
+        }
+
+        arr[r] = pivot;
+
+        if (l < right) {
+            quickSortLast(arr, l + 1, right);
+        }
+        if (r > left) {
+            quickSortLast(arr, left, r - 1);
+        }
+
+
+    }
+
+    public static void testqquickSort(int[] arr, int left, int right) {
+
+        if (left >=right){
+            return;
+        }
+        int l = left;
+        int r = right;
+        int pivot = arr[l];
+
+        while (l < r) {
+
+            while (l < r && arr[r] >= pivot) {
+                r--;
+            }
+
+            if (l < r) {
+                arr[l] = arr[r];
+            }
+
+            while (l < r && arr[l] <= pivot) {
+                l++;
+            }
+
+            if (l < r) {
+                arr[r] = arr[l];
+            }
+
+        }
+
+
+        arr[l] = pivot;
+
+        if (l < right) {
+            testqquickSort(arr, l + 1, right);
+        }
+
+        if (r > left) {
+            testqquickSort(arr, left, r - 1);
+        }
+
+
+    }
+
+}
