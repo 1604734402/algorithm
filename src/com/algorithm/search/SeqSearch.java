4897e27e4b5a13f39708f6c395267491f79b49d1
home <1604734402@qq.com>
2021-01-10 12:55:31 +0800

    同步 本地项目

--- src/com/algorithm/search/SeqSearch.java
+++ src/com/algorithm/search/SeqSearch.java
@@ -1,33 +1,32 @@
 package com.algorithm.search;
 
 /**
- * @auther liuyiming
- * @date 2021/1/8
- *
  * 线性查找
- *
- * 从前向后顺序查找和val相同的数，
- * 若找到，则返回第一个的下标，
- * 若遍历后找不到，则返回-1
  */
 public class SeqSearch {
 
     public static void main(String[] args) {
-        int[] arr = {1,5,8,9,12,-1,516,887};
-
-        System.out.println(seqSearch(arr,-1));
+        int arr[] = {1,9,11,-1,34,89};
+        int i = seqSearch(arr,34);
+        System.out.println(i); 
     }
 
-    public static int seqSearch(int[] arr,int val){
+    /**
+     * 线性查找
+     * 根据数组，所查找的值，如果有所查找的，返回下标，否则返回-1
+     *
+     * @param arr
+     * @param value
+     * @return
+     */
+    public static int seqSearch(int[] arr,int value){
 
         for (int i = 0; i < arr.length; i++) {
-            if (val == arr[i]){
+            if (arr[i] == value){
                 return i;
             }
         }
-
         return -1;
+
     }
-
-
 }
